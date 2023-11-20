f97db8ce89d590199803033bd7a1a19f9b3d6309
Aleksandr Litskevich <a.litskevich@lightmap.com>
2023-11-20 21:02:23 +0400

    Added trails

---
+++ Assets/Scripts/FX/Trails.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace FX
+{
+    public class Trails : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+        [SerializeField] private TrailRenderer _trailRenderer;
+
+        private void Awake()
+        {
+            
+        }
+    }
+}
