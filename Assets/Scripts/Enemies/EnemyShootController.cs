56e963acb3e9f80229187120f14a86e2454a919a
Pavel-Lopatin <88245663+Pavel-Lopatin@users.noreply.github.com>
2023-12-01 00:21:09 +0500

    Test

---
+++ Assets/Scripts/Enemies/EnemyShootController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShootController : MonoBehaviour
+{
+    [SerializeField] private float _reloadTime;
+    [SerializeField] private Transform _firePoint;
+    [SerializeField] private EnemyShell _shellPrefab;
+
+
+    private bool _canShoot = false;
+    private float _reloadTimer;
+
+    private void Start()
+    {
+        _reloadTimer = _reloadTime;
+    }
+
+    private void Update()
+    {
+        if (!_canShoot) UpdateReloadTimer();
+        else
+        {
+            EnemyShell shell = Instantiate(_shellPrefab, _firePoint.position, _firePoint.rotation);
+
+            _canShoot = false;
+            _reloadTimer = _reloadTime;
+
+        }
+
+
+    }
+
+    private void UpdateReloadTimer()
+    {
+        _reloadTimer -= Time.deltaTime;
+        if (_reloadTimer <= 0)
+        {
+            _canShoot = true;
+            return;
+        }
+    }
+
+}
